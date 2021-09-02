5c56505e4e4a2ce5c5bac4064b1e19e282977da8
sayandey09 <sayandey.ma@gmail.com>
2021-09-02 23:19:42 +0530

    first commit

---
+++ github.js
@@ -0,0 +1,26 @@
+class GitHub {
+  constructor() {
+    this.clint_id = "9a259f387d31eac6c705";
+    this.clint_secret = "06762a911ad94e6d50ee2bd680d80487f31a9ef6";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
+}
